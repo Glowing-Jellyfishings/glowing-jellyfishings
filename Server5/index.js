02dca313c57bfd861bbfa7fbebfdfda9d5c3a507
Glowing Jellyfishing <glowingjellyfishing@outlook.com>
2025-08-15 06:33:06 +0000

    Fixed more code.

--- Server5/index.js
+++ Server5/index.js
@@ -8,4 +8,4 @@ app.use(express.json());
 app.get('/callback', oauth.handleCallback);
 app.post('/webhook', webhook.handleWebhook);
 
-app.listen(9995, () => console.log('🚀 Bot running on http://localhost:3000'));
+app.listen(9995, () => console.log('🚀 Bot running on http://localhost:9995'));
