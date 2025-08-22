d6f6a729c5da9dcb362162a069a92dd3b1ac5d0e
Glowing Jellyfishing <glowingjellyfishing@outlook.com>
2025-08-22 07:19:22 +0000

    Added glow.

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (req, res) => {
-  const filePath = path.join(__dirname, '../views/index.html');
+  const filePath = path.join(__dirname, '../site/index.html');
   fs.readFile(filePath, 'utf8', (err, html) => {
     if (err) {
       res.status(500).send('Error loading page');
