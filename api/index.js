72ac906b130777a7be45adc13bdd7e9413025472
Glowing Jellyfishing <glowingjellyfishing@outlook.com>
2025-08-22 17:12:48 +1000

    Update index.js
    Signed-off-by: Glowing Jellyfishing <glowingjellyfishing@outlook.com>

--- api/index.js
+++ api/index.js
@@ -3,7 +3,12 @@ const path = require('path');
 
 module.exports = (req, res) => {
   const filePath = path.join(__dirname, '../views/index.html');
-  const html = fs.readFileSync(filePath, 'utf8');
-  res.setHeader('Content-Type', 'text/html');
-  res.status(200).send(html);
+  fs.readFile(filePath, 'utf8', (err, html) => {
+    if (err) {
+      res.status(500).send('Error loading page');
+    } else {
+      res.setHeader('Content-Type', 'text/html');
+      res.status(200).send(html);
+    }
+  });
 };
